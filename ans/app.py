58cea32ad6d33a841eda058e0b74769fd58c74e6
Code <test2.klassic@outlook.com>
2025-09-03 08:10:45 +0000

    update

--- ans/app.py
+++ ans/app.py
@@ -1,22 +1,22 @@
+# This is a simple Flask application to serve the ANSCOM corporate website.
+# The `render_template` function will automatically look for HTML files in a 'templates' folder.
+
 from flask import Flask, render_template
 
-app = Flask(__name__)
+# Initialize the Flask application.
+# The static_folder is set to 'static' to serve static assets like images,
+# and the template_folder is set to 'templates' for HTML files.
+app = Flask(__name__, static_folder='static', template_folder='templates')
 
 @app.route('/')
-def index():
+def home():
+    """
+    This is the main route for the website.
+    It renders the index.html file, which contains the complete website.
+    """
     return render_template('index.html')
 
-@app.route('/services')
-def services():
-    return render_template('services.html')
-
-@app.route('/about')
-def about():
-    return render_template('about.html')
-
-@app.route('/contact')
-def contact():
-    return render_template('contact.html')
-
 if __name__ == '__main__':
+    # Run the application in debug mode for development.
+    # The debug flag allows for automatic code reloading and a debugger.
     app.run(debug=True)
